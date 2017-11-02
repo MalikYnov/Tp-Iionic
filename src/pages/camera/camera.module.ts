60ee97c8c6f2ae063cac226ec715b1381a048792
malik10 <malik.verdiere@ynov.com>
2017-11-02 10:22:08 +0100

    test

--- src/pages/camera/camera.module.ts
+++ src/pages/camera/camera.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CameraPage } from './camera';
 import { Camera } from '@ionic-native/camera';
+import { MediaCapture} from '@ionic-native/media-capture';
 @NgModule({
   declarations: [
     CameraPage,
   ],
   imports: [
-    IonicPageModule.forChild(CameraPage),
-    Camera
+    IonicPageModule.forChild(CameraPage)
   ],
   providers:[
-    Camera
+    Camera,
+    MediaCapture
   ]
 })
 export class CameraPageModule {}
--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
 /**
  * Generated class for the CameraPage page.
  *
@@ -13,13 +15,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-camera',
   templateUrl: 'camera.html',
 })
+
 export class CameraPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    imgView:String ;
+    VideoView:string;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CameraPage');
-  }
+    optionsVideo: CaptureImageOptions = { limit: 3 };
+    options: CameraOptions = {
+  
+        quality: 100,
+  
+        destinationType: this.camera.DestinationType.DATA_URL,
+  
+        encodingType: this.camera.EncodingType.JPEG,
+  
+        mediaType: this.camera.MediaType.PICTURE,
+  
+        saveToPhotoAlbum : true
+      }
+  
+      base64ToGallery:Base64ToGallery;
+  
+    
+      constructor(private camera: Camera, private mediaCapture:MediaCapture) {
+    }
+  
+  
+  
+    btnCameraTapped(event) {
+      // That's right, we're pushing to ourselves!
+      this.camera.getPicture(this.options).then((imageData) => {
+  
+       // imageData is either a base64 encoded string or a file URI
+  
+       // If it's base64:
+  
+       this.imgView= 'data:image/jpeg;base64,' + imageData;
+       this.base64ToGallery.base64ToGallery(imageData, { prefix: '_img' }).then(
+  
+          res => console.log('Saved image to gallery ', res),
+  
+          err => console.log('Error saving image to gallery ', err)
+  
+      );
+  
+      }, (err) => {
+  
+       // Handle error
+  
+      });
+  
+    }
 
-}
+
+    btnTakeVideoTapped(event) {
+      
+      this.mediaCapture.captureImage(this.optionsVideo)
+        .then(
+          (data: MediaFile[]) => console.log(data),
+          (err: CaptureError) => console.error(err)
+        );
+      
+        }
+  
+  }
